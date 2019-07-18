56e4a2d06751e3115f4d612a08a49f3189715d51
YellowTulipShow <1426689530@qq.com>
2019-07-18 21:31:27 +0800

    change JSON

--- resource_manager/js/base.js
+++ resource_manager/js/base.js
@@ -1,46 +1,55 @@
 /* 原生支持 JSON */
 (function() {
     /* https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/JSON */
-    if (!window.JSON) {
-        window.JSON = {
-            parse: function(sJSON) {
-                return eval('(' + sJSON + ')');
-            },
-            stringify: (function() {
-                var toString = Object.prototype.toString;
-                var isArray = Array.isArray || function(a) {
-                    return toString.call(a) === '[object Array]';
-                };
-                var escMap = { '"': '\\"', '\\': '\\\\', '\b': '\\b', '\f': '\\f', '\n': '\\n', '\r': '\\r', '\t': '\\t' };
-                var escFunc = function(m) {
-                    return escMap[m] || '\\u' + (m.charCodeAt(0) + 0x10000).toString(16).substr(1);
-                };
-                var escRE = /[\\"\u0000-\u001F\u2028\u2029]/g;
-                return function stringify(value) {
-                    if (value == null) {
-                        return 'null';
-                    } else if (typeof value === 'number') {
-                        return isFinite(value) ? value.toString() : 'null';
-                    } else if (typeof value === 'boolean') {
-                        return value.toString();
-                    } else if (typeof value === 'object') {
-                        if (typeof value.toJSON === 'function') {
-                            return stringify(value.toJSON());
-                        } else if (isArray(value)) {
-                            var res = '[';
-                            for (var i = 0; i < value.length; i++) res += (i ? ', ': '') + stringify(value[i]);
-                            return res + ']';
-                        } else if (toString.call(value) === '[object Object]') {
-                            var tmp = [];
-                            for (var k in value) {
-                                if (value.hasOwnProperty(k)) tmp.push(stringify(k) + ': ' + stringify(value[k]));
-                            }
-                            return '{' + tmp.join(', ') + '}';
-                        }
+    var ToObject = function(str_json) {
+        return eval('(' + sJSON + ')');
+    }
+    var ToString = (function() {
+        var toString = Object.prototype.toString;
+        var isArray = Array.isArray || function(a) {
+            return toString.call(a) === '[object Array]';
+        };
+        var escMap = { '"': '\\"', '\\': '\\\\', '\b': '\\b', '\f': '\\f', '\n': '\\n', '\r': '\\r', '\t': '\\t' };
+        var escFunc = function(m) {
+            return escMap[m] || '\\u' + (m.charCodeAt(0) + 0x10000).toString(16).substr(1);
+        };
+        var escRE = /[\\"\u0000-\u001F\u2028\u2029]/g;
+        return function stringify(value) {
+            if (value == null) {
+                return 'null';
+            } else if (typeof value === 'number') {
+                return isFinite(value) ? value.toString() : 'null';
+            } else if (typeof value === 'boolean') {
+                return value.toString();
+            } else if (typeof value === 'object') {
+                if (typeof value.toJSON === 'function') {
+                    return stringify(value.toJSON());
+                } else if (isArray(value)) {
+                    var res = '[';
+                    for (var i = 0; i < value.length; i++) res += (i ? ', ': '') + stringify(value[i]);
+                    return res + ']';
+                } else if (toString.call(value) === '[object Object]') {
+                    var tmp = [];
+                    for (var k in value) {
+                        if (value.hasOwnProperty(k)) tmp.push(stringify(k) + ': ' + stringify(value[k]));
                     }
-                    return '"' + value.toString().replace(escRE, escFunc) + '"';
-                };
-            })()
+                    return '{' + tmp.join(', ') + '}';
+                }
+            }
+            return '"' + value.toString().replace(escRE, escFunc) + '"';
+        };
+    })();
+    if (!this.JSON) {
+        this.JSON = {
+            parse: ToObject,
+            stringify: ToString,
+            ToObject: ToObject,
+            ToString: ToString,
+        };
+    } else {
+        this.JSON = {
+            ToObject: this.JSON.parse,
+            ToString: this.JSON.stringify,
         };
     }
 })();
