e98fda4e041efee31ce6d407bbf1f9762185e0f3
YellowTulipShow <1426689530@qq.com>
2020-01-09 11:46:35 +0800

    create test dbStore.js

---
+++ assets/js/dbStore.js
@@ -0,0 +1,46 @@
+(function() {
+    var ObjectIndexedDB = this.indexedDB ||
+        this.webkitIndexedDB ||
+        this.mozIndexedDB ||
+        this.msIndexedDB;
+
+    var dbStore = function(argumentConfig) {
+        this.config = this.__DefaultConfig__(argumentConfig);
+        this.__init__();
+    }
+    dbStore.prototype = {
+        __init__: function() {
+        },
+        __DefaultConfig__: function(argumentConfig) {
+
+            var config = {
+                'name': 'project',
+            };
+            return $.extend(config, argumentConfig);
+        },
+        Insert: function(model) {
+            var self = this;
+            return true;
+        },
+        Edit: function(model) {
+            var self = this;
+            return true;
+        },
+        Delete: function(selectMethod) {
+            var self = this;
+            return true;
+        },
+        Query: function(selectMethod) {
+            var self = this;
+            return [];
+        },
+    };
+    dbStore.prototype.constructor = dbStore;
+    dbStore.IsSupportIndexedDB = function() {
+        if (ObjectIndexedDB) {
+            return true;
+        }
+        return false;
+    }
+    this.dbStore = dbStore;
+})();
