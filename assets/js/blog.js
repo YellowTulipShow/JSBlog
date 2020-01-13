f09ba5c1bf3df4cd27a0bb5bb2a4aa6327f11fa8
YellowTulipShow <1426689530@qq.com>
2020-01-13 14:44:34 +0800

    增加自动保存选项

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -22,6 +22,7 @@
     var Blog = function(argumentConfig) {
         this.config = this.__DefaultConfig__(argumentConfig);
         this.IAPI = FactoryGitAPI(this.config.platform, this.config.owner, this.config.repo);
+        this.db = null;
         this.__init__();
     }
     Blog.prototype = {
@@ -30,32 +31,75 @@
                 platform: default_platform,
                 owner: default_owner,
                 repo: default_repo,
+                isSave: false,
+                dbOpenError: function() {},
+                dbOpenSuccess: function() {},
             };
             return $.extend(config, argumentConfig);
         },
         __init__: function() {
             var self = this;
+            self.__init_database__();
+        },
+        __init_database__: function() {
+            var self = this;
+            if (!self.config.isSave) {
+                return;
+            }
+            self.db = new dbStore({
+                "name": self.DBName(),
+                "fields": [
+                    { name: "path", isSign: true, isUnique: true, },
+                    { name: "value" },
+                ],
+                dbOpenError: function() {
+                    self.config.dbOpenError();
+                },
+                dbOpenSuccess: function() {
+                    self.config.dbOpenSuccess();
+                },
+            });
+        },
+        DBName: function() {
+            var self = this;
+            return "{platform}/{owner}/{repo}".format(self.config);
+        },
+        RequestGet: function(url, callback) {
+            var self = this;
+            var IAPI = self.IAPI;
+            var request = new WebRequest({});
+            request.Send({
+                url: url,
+                type: "GET",
+                data: {},
+                dataType: "json",
+                success: function(json) {
+                    callback(json);
+                },
+                error: function(XMLHttpRequest_obj, textStatus_obj, errorThrown_obj) {
+                    callback(null);
+                },
+                complete: function(XMLHttpRequest_obj, TypeStatus) {
+                    console.log("请求完成: url:", url);
+                },
+            });
         },
         RequestUser: function(callback) {
             var self = this;
             var IAPI = self.IAPI;
             var url = IAPI.toUserURL();
-            var request = new WebRequest({});
-            request.Send({
-                url: url,
-                type: "GET",
-                data: "",
-                dataType: "json",
-                success: function(json) {
-                    var model = IAPI.toUserModel(json);
-                    callback(model);
-                },
-                error: function(XMLHttpRequest, textStatus, errorThrown) {
-                    console.log("error:", XMLHttpRequest);
-                },
-                complete: function(XMLHttpRequest, TypeStatus) {
-                    console.log("请求完成: url:", url);
-                },
+
+            if (self.db != null) {
+                db.Query(function(model) {
+                    return model.path == url;
+                }, function(result_list) {
+                    callback(result_list);
+                });
+                return;
+            }
+            self.RequestGet(url, function(json) {
+                var model = IAPI.toUserModel(json);
+                callback(model);
             });
         },
         RequestRepoContent: function(callback, path) {
@@ -63,22 +107,17 @@
             var self = this;
             var IAPI = self.IAPI;
             var url = IAPI.toRepoContentURL(path);
-            var request = new WebRequest({});
-            request.Send({
-                url: url,
-                type: "GET",
-                data: {},
-                dataType: "json",
-                success: function(json) {
-                    var model = IAPI.toRepoContentModel(json);
-                    callback(model);
-                },
-                error: function(XMLHttpRequest_obj, textStatus_obj, errorThrown_obj) {
-                    callback(null);
-                },
-                complete: function(XMLHttpRequest_obj, TypeStatus) {
-                    console.log("请求完成: url:", url);
-                },
+            if (self.db != null) {
+                db.Query(function(model) {
+                    return model.path == url;
+                }, function(result_list) {
+                    callback(result_list);
+                });
+                return;
+            }
+            self.RequestGet(url, function(json) {
+                var model = IAPI.toRepoContentModel(json);
+                callback(model);
             });
         },
         RequestRepoFile: function(callback, path) {
@@ -86,22 +125,17 @@
             var self = this;
             var IAPI = self.IAPI;
             var url = IAPI.toRepoContentURL(path);
-            var request = new WebRequest({});
-            request.Send({
-                url: url,
-                type: "GET",
-                data: {},
-                dataType: "json",
-                success: function(json) {
-                    var model = IAPI.toRepoFileModel(json);
-                    callback(model);
-                },
-                error: function(XMLHttpRequest_obj, textStatus_obj, errorThrown_obj) {
-                    callback(null);
-                },
-                complete: function(XMLHttpRequest_obj, TypeStatus) {
-                    console.log("请求完成: url:", url);
-                },
+            if (self.db != null) {
+                db.Query(function(model) {
+                    return model.path == url;
+                }, function(result_list) {
+                    callback(result_list);
+                });
+                return;
+            }
+            self.RequestGet(url, function(json) {
+                var model = IAPI.toRepoFileModel(json);
+                callback(model);
             });
         },
     };
